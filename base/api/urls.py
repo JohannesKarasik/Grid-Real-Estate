cd8714cc26c177859e07c7c939105c3c33b7427b
JohannesKarasik <johanneskarasikweb@gmail.com>
2024-11-22 13:46:46 -0500

    Update urls.py

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,8 +1,1 @@
-from django.urls import path
-from . import views
 
-urlpatterns = [
-    path('', views.getRoutes),
-    path('rooms/', views.getRooms),
-    path('rooms/<str:pk>/', views.getRoom),
-]
