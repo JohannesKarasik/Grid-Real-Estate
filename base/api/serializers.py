bfb112cb3f1dcea16395fed6274931d6c15227b2
JohannesKarasik <johanneskarasikweb@gmail.com>
2024-11-22 13:46:57 -0500

    Update serializers.py

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,8 +1,1 @@
-from rest_framework.serializers import ModelSerializer
-from base.models import Room
 
-
-class RoomSerializer(ModelSerializer):
-    class Meta:
-        model = Room
-        fields = '__all__'
